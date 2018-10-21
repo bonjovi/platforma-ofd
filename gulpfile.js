de84134b3c90df9afb1b038f1f22dcc1643c8dc0
Roman Kalashnikov <rk@MacBook-Pro-Roman.local>
2018-10-21 16:39:25 +0300

    Changed files structure and assambly conditions

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ var gulp = require('gulp'),
     prefix = require('gulp-autoprefixer'),
     browserSync = require('browser-sync').create();
 
-gulp.task('blocks', function() {
-    return gulp.src('app/scss/blocks/**/*.scss')
-        .pipe(plumber())
-    	.pipe(concatcss("_blocks.scss"))
-    	.pipe(gulp.dest('app/scss/build/'))
-        .pipe(browserSync.stream());
-});
+// gulp.task('blocks', function() {
+//     return gulp.src('app/scss/blocks/**/*.scss')
+//         .pipe(plumber())
+//     	.pipe(concatcss("_blocks.scss"))
+//     	.pipe(gulp.dest('app/scss/build/'))
+//         .pipe(browserSync.stream());
+// });
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/main.scss')
@@ -26,11 +26,11 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('clean', function () {  
-    return gulp.src('app/scss/build/_blocks.scss', {read: false})
-        .pipe(clean())
-        .pipe(browserSync.stream());
-});
+// gulp.task('clean', function () {  
+//     return gulp.src('app/scss/build/_blocks.scss', {read: false})
+//         .pipe(clean())
+//         .pipe(browserSync.stream());
+// });
 
 
 gulp.task('watch', function() {
@@ -38,5 +38,5 @@ gulp.task('watch', function() {
         server: "./app"
     });
 
-    gulp.watch('app/scss/blocks/**/*.scss', gulp.series('clean', 'blocks', 'sass'));
+    gulp.watch('app/scss/blocks/**/*.scss', gulp.series('sass'));
 });
