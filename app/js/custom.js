0dd7aa31e51c1a68d189074bc271a81b3fb9a02d
Roman Kalashnikov <roman-kalashnikoff@mail.ru>
2018-10-23 17:56:47 +0300

    Added main content

--- app/js/custom.js
+++ app/js/custom.js
@@ -3,4 +3,10 @@ $(function() {
         $('.formarea').addClass('formarea_opened');
         $('.page').addClass('page_blured');
     }, 1000);
+
+    $('.shevron').on('click', function() {
+        $(this).toggleClass('shevron_opened');
+        $(this).parent().find('.section__preview').toggleClass('section__preview_closed');
+        $(this).parent().find('.section__postview').toggleClass('section__postview_closed');
+    });
 });
